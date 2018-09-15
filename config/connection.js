ef25089f0f332e5edfb10b1803c390f905d967a7
Malkons <marc.alkons@gmail.com>
2018-09-15 14:05:09 -0700

    trying to fix connection error

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+
+
 var connection;
+
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
@@ -13,16 +17,8 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1234",
-  database: "burger_db"
-});
-
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
