f41dafac80f046727b83d987739914b2d146e091
Malkons <marc.alkons@gmail.com>
2018-09-15 17:02:52 -0700

    deleted second button, added bootstrap, fixed name on eaten burgers list

--- config/connection.js
+++ config/connection.js
@@ -12,8 +12,8 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "MyNewPass",
-    database: "burgers_db"
+    password: "1234",
+    database: "burger_db"
   });
 };
 
